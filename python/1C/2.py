f0b645c2ff2f0a5003ecb65566cdb42894a72326
AlanLatte <shukurovfoteh@gmail.com>
2019-03-03 21:20:18 +0300

    EnvyPlop

--- python/1C/2.py
+++ python/1C/2.py
@@ -1,17 +1,14 @@
-string = 'Петрова И.С 555-44-77'
 massive =   [
                 ['ПеРрова', 'И.С', ['555','44','76']],
                 ['Перова', 'С.С', ['555','44','77']],
-                ['Петрова', 'И.С', ['555','44','77']]
+                ['Петрова', 'И.С', ['555','44','77']],
+                ['ПетDова', 'И.С', ['555','44','74']]
             ]
-
-correct = []
+data = []
 for i in range(len(massive)):
-    correct.append(massive[i][-1][-1])
-print(correct)
-d = []
-for i in correct:
-    d.append(correct.index(i))
-    if d > 1:
-        for q in range(2):
-            print(massive[lol])
+	data.append(int(massive[i][-1][-1]))
+
+
+# for i in range(len(data)):
+# 	for q in 
+	
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-string = "aaffafasdas"
-data, massive = [], []
-for i in string:
-    if i in massive:
-        data[massive.index(i)]+=1
-    else:
-        data.append(1)
-        massive.append(i)
-print(massive)
-print(data)
+a =   		[
+                ['ПеРрова', 'И.С', ['555','44','76']],
+                ['Перова', 'С.С', ['555','44','77']],
+                ['Петрова', 'И.С', ['555','44','77']],
+                ['ПетDова', 'И.С', ['555','44','74']]
+            ]
+while len(a)>0:
+	i = 1
+	q = 0
+	while i-1<len(a[-1][-1])-1:
+		if a[-1][0]==[-1][-1][i]:
+			q=2
+			print(a[i])
+			del a[i]
+			i-=1
+		i+=1	
+	if q == 2:
+		print(a[0])
+	del a[0]
+
+print(a[-1][-1][-1])
