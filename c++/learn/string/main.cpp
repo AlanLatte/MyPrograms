4235717a0b15d270b4ee43cc79d3844629fd6128
AlanLatte <shukurovfoteh@gmail.com>
2019-03-11 18:29:59 +0300

    work with string c++

--- c++/learn/string/main.cpp
+++ c++/learn/string/main.cpp
@@ -1,2 +1,20 @@
 #include <string>
 #include <iostream>
+
+string name;
+std::cout << "Enter your name" << flush;
+cin >> name;
+getline(cin,name);
+
+if (name = "")
+{
+	cout 	<< "Empty name"
+				<< "assigning default\n";
+	name = "John";
+}
+else
+{
+	std::cout << "Thank you, " << '\n'
+	<< "for running"
+	<< "this simple program!" << endl;
+}
