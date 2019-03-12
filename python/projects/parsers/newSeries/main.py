211b106bda7d0809074e708129d113ac16f3ec44
AlanLatte <shukurovfoteh@gmail.com>
2019-03-12 15:46:55 +0300

    Update_NewSeries теперь выводятся новые серии

--- python/projects/parsers/newSeries/main.py
+++ python/projects/parsers/newSeries/main.py
@@ -8,42 +8,56 @@ def parse_anidub(url, index):
                 soup = bs(request.content,'html.parser')
                 divs = soup.find_all('div', attrs={'title'})
                 data = re.search(r'>(.*)<', str(divs)).group()
-                text_write(data=data)
-                open_url(url=url)
+                data_write(data=f'{index}'+data)
             else:
                 print('Server fall')
-        except None:
-            text = 'line '+str(index+1)+' in urls.txt was broken'
-            text_write(data=text)
+        except:
+            text = 'line '+str(index)+' in urls.txt was broken'
+            data_write(data=f'{index}'+text)
             print(text)
 
-def open_url(url):
+
+def open_url(url, index):
     try:
         webbrowser.open(str(url))
     except:
         print('An error occurred while opening the link url')
 
+def data_check():
+    BEFORE = clean_data(get_result())
+    result_clear()
+    site_detect(url=clean_data(get_urls()))
+    AFTER = clean_data(get_result())
+    if BEFORE != AFTER:
+        result=list(set(AFTER) - set(BEFORE))
+        print(result)
+    else:
+        print('New series did not come out')
 
-
-def text_write(data):
+def data_write(data):
     with open('result.txt', mode='a', encoding='utf=8') as result:
         result.write(data+'\n')
 
 def site_detect(url):
-    clean_data(url)
     for i in url:
         index = url.index(i)
         if re.search(r'www.anilibria', i):
             print('Please paste only Anidub url')
         else:
-            parse_anidub(url=i, index=index)
+            parse_anidub(url=i, index=index+1)
 
 def change_dir():
     os.chdir(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))))
 
-def text_data():
+def result_clear():
     with open('result.txt', mode='w') as clean:
         clean.write
+
+def get_result():
+    with open('result.txt', mode='r') as data:
+        return data.read().split('\n')
+
+def get_urls():
     with open('urls.txt', mode='r') as data:
         return data.read().split('\n')
 
@@ -52,10 +66,10 @@ def clean_data(url):
     if index:
         del url[index]
     return url
+
 def main():
     change_dir()
-    site_detect(url=text_data())
-
+    data_check()
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-def a():
-    c=[1,2]
-    d=[3,4]
-    return c,d
-a()[0]
+BEFORE = ['1> Дороро / Dororo [08 из 12+]<', '2> Моб Психо 100 ТВ-2 / Mob Psycho 100 TV-2 [10 из 12]<', '3> О моём перерождении в слизь / Tensei Shitara Slime Datta Ken [22 из 25]<', '4> Восхождение Героя щита / Tate no Yuusha no Nariagari [09 из 25]<']
+
+AFTER = ['1> Дороро / Dororo [09 из 12+]<', '2> Моб Психо 100 ТВ-2 / Mob Psycho 100 TV-2 [10 из 12]<', '3> О моём перерождении в слизь / Tensei Shitara Slime Datta Ken [22 из 25]<', '4> Восхождение Героя щита / Tate no Yuusha no Nariagari [09 из 25]<']
+
+
+result=list(set(AFTER) - set(BEFORE))
+print(result[0][0])
