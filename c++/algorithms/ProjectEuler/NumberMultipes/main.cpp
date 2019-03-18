161c7036a0ec1701e415833ea08203b4342ecc54
AlanLatte <shukurovfoteh@gmail.com>
2019-03-18 20:12:43 +0300

    Project Euler Task1 Update

--- c++/algorithms/ProjectEuler/NumberMultipes/main.cpp
+++ c++/algorithms/ProjectEuler/NumberMultipes/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+/*
+						TASK:
+						1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+int logic(int range){
+	int sum=0;
+	for (size_t i = 0; i < range; i++) {
+		if (i%3==0){
+			sum+=i;
+		}else if (i%5==0){
+			sum+=i;
+		}
+	}
+	return sum;
+}
+
 int main(int argc, char const *argv[]) {
-	std::cout << "hello world" << '\n';
-	return 0;
+	int range=1000;
+	std::cout << logic(range) << '\n';
 }
