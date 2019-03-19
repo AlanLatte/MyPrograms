a68dd4b946f22e4a4b68a405f05c218974f539cc
AlanLatte <shukurovfoteh@gmail.com>
2019-03-19 11:57:40 +0300

    c++

--- c++/algorithms/ProjectEuler/3_LPF/main.cpp
+++ c++/algorithms/ProjectEuler/3_LPF/main.cpp
@@ -2,24 +2,47 @@
 			Task:
 			3
 The prime factors of 13195 are 5, 7, 13 and 29.
-
 What is the largest prime factor of the number 600851475143 ?
 */
-
 #include <iostream>
+///////////////////////////////////////////////////////////////////////////////
+void    print_prime_factors( int    n )
+{
+    for ( int i = 2; i*i <= n; ++i )
+    {
+        if  (
+                n % i   ==  0
+            )
+        {
+            std::cout   << i
+                        << ' ';
 
-int lpf(int number){
-	int *result = new int[number+1];
-	
-	std::cout << result << '\n';
-	return 0;
+            while( n % i    ==  0 )
+            {
+                n   /=  i;
+            }
+        }//if
+    }
+
+    if( n != 1 )
+    {
+        std::cout   <<  n;
+    }
+
+    std::cout   <<  std::endl
+                <<  "finish"
+                <<  std::endl
+                <<  std::endl;
 }
+///////////////////////////////////////////////////////////////////////////////
+int     main()
+{
+    for(;;)
+    {
+        int     n{};
+        std::cout   <<  "n = ";
+        std::cin    >>  n;
 
-
-int main(int argc, char const *argv[]) {
-	// int number = 600851475143;
-	int number = 10;
-	// std::cout << lpf(number) << '\n';
-	lpf(number);
-	return 0;
+        print_prime_factors(n);
+    }//for
 }
