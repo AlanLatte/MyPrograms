82ef1f382f09d98d918d2c69f3a45ef186014995
AlanLatte <47124982+AlanLatte@users.noreply.github.com>
2019-03-28 14:44:27 +0300

    Update main.py

--- python/projects/algorithm/morse/main.py
+++ python/projects/algorithm/morse/main.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from data import logic
 def main():
     print(logic())
 if __name__ == '__main__':
-=======
-from data import logic
-def main():
-    print(logic())
-if __name__ == '__main__':
->>>>>>> Some error have exists
     main()
