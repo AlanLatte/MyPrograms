36d758fcf71435237e795a8f3745cca5c7fc0dd3
AlanLatte <47124982+AlanLatte@users.noreply.github.com>
2019-03-28 14:45:11 +0300

    Update generate.py

--- python/projects/algorithm/qrcode/generate.py
+++ python/projects/algorithm/qrcode/generate.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import qrcode,image,uuid
 def gen(data):
     qrcode.make(data=data).save(''.join(a for a in str(uuid.uuid4()) if a!="-")+".png")
-=======
-import qrcode,image,uuid
-def gen(data):
-    qrcode.make(data=data).save(''.join(a for a in str(uuid.uuid4()) if a!="-")+".png")
->>>>>>> Some error have exists
