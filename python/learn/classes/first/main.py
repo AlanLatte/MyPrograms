cd70158c01c62a33e96a99c8284dc16c025ec0d0
AlanLatte <47124982+AlanLatte@users.noreply.github.com>
2019-06-26 17:44:19 +0300

    [*] FIX

--- python/learn/classes/first/main.py
+++ python/learn/classes/first/main.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 class A:
     def hello(self):
         return 'hello world my name is ' + name
 name = 'Amal'
 print(A().hello())
-=======
-class A:
-    def hello(self):
-        return 'hello world my name is ' + name
-name = 'Amal'
-print(A().hello())
->>>>>>> Some error have exists
---
+++ python/learn/tests/1.py
@@ -0,0 +1,4 @@
+def a():
+    return lambda x: 2+x
+
+a()(10)
